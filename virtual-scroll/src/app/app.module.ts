f358be942f64c53f090042dabbbb66db7e1b0804
tsava <tsava@exoft.net>
2019-06-11 23:40:12 +0300

    Added angular materials

--- virtual-scroll/src/app/app.module.ts
+++ virtual-scroll/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VirtualScrollComponent } from './components/virtual-scroll/virtual-scroll.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualScrollComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
