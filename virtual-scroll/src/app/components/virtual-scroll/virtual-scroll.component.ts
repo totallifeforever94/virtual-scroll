793fbe7248b9ca211654f1f268d2444e06e13a4b
tsava <tsava@exoft.net>
2019-06-11 23:47:47 +0300

    Added faker.js

--- virtual-scroll/src/app/components/virtual-scroll/virtual-scroll.component.ts
+++ virtual-scroll/src/app/components/virtual-scroll/virtual-scroll.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as faker from 'faker';
+
 @Component({
   selector: 'app-virtual-scroll',
   templateUrl: './virtual-scroll.component.html',
   styleUrls: ['./virtual-scroll.component.scss']
 })
 export class VirtualScrollComponent implements OnInit {
+  public people: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.people = Array(100).fill(1).map(_ => {
+      return {
+        name: faker.name.findName(),
+        bio: faker.hacker.phrase()
+      };
+    });
   }
 
 }
